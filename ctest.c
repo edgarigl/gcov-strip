290a113e4e2f38d3b5d46595a075d4fd7c2c4013
Edgar E. Iglesias <edgar.iglesias@amd.com>
2024-09-09 16:45:41 +0200

    hax
    Signed-off-by: Edgar E. Iglesias <edgar.iglesias@amd.com>

--- ctest.c
+++ ctest.c
@@ -6,11 +6,13 @@ volatile int x;
 void func(int a, int b, int c)
 {
     if (a > 10 && (b || c)) {
+        printf("SET\n");
         x = 10;
     }
 }
 
 int main(int argc, char *argv[]) {
-    func(10, 20, 0);
+    func(11, 20, 0);
+    printf("done\n");
 	return 0;
 }
